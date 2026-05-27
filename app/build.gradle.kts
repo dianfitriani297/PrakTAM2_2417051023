74358e00cc7cc2f18c5e2bd7f2ddf1a8689ba82a
dianfitriani297 <dianfitrianipuhun@gmail.com>
2026-05-27 16:20:48 +0700

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,5 +59,4 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
 }
